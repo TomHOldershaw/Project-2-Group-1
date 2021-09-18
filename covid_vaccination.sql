9c217bf3c12ff1d174e3e47c3fba6968221ef9b1
chokison79 <85762203+chokison79@users.noreply.github.com>
2021-09-18 13:05:05 +0100

    Add files via upload

--- covid_vaccination.sql
+++ covid_vaccination.sql
@@ -1,10 +1,10 @@
 -- Create a new table
 CREATE TABLE Vaccination_Record (
-  Index Int Primary key,
-  country VARCHAR(30) NOT NULL,
-  Population INT,
+  Index Int,
+  country VARCHAR NOT NULL,
+  Population DEC,
   GDP_per_capita DEC,
-  Doses INT,
+  Doses DEC,
   pc_1_Dose DEC,
   pc_2_Doses DEC,
   World_region VARCHAR
